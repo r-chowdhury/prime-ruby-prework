902949bff2e8a10e260b69d24949aaf304611435
ryhan.chowdhury3@gmail.com <ryhan.chowdhury3@gmail.com>
2018-06-10 18:57:48 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,21 @@
 # Add  code here!
+def prime?(num)
+  if num <= 2 
+    return false 
+  end 
+  
+  if num % 2 == 0 
+    return false
+  end
+  
+  i = 2 
+  limit = num / i 
+  while i < limit
+    if num % i == 0 
+      return false 
+    end 
+    i += 1 
+    limit = num / i 
+  end 
+  return true
+end
